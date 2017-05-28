1ca30c1a1752477a2538d6c5151f9020322b1aff
zxcv5500 <zxcv5500@gmail.com>
2017-05-29 03:55:27 +0900

    한국어 커밋 테스트

--- app/src/main/java/tistory/zxcv5500/typingandroidprogrammingconquest/AndExam.java
+++ app/src/main/java/tistory/zxcv5500/typingandroidprogrammingconquest/AndExam.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class AndExam extends Activity {
+	static final int SETTING_ACTIVITY = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
