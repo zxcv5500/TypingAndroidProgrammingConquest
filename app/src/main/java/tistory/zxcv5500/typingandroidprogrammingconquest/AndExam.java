efc7ec78bf18a6f930da51c8fab51407699c4f9c
zxcv5500 <zxcv5500@gmail.com>
2017-05-29 03:54:23 +0900

    first commit

---
+++ app/src/main/java/tistory/zxcv5500/typingandroidprogrammingconquest/AndExam.java
@@ -0,0 +1,13 @@
+package tistory.zxcv5500.typingandroidprogrammingconquest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndExam extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.andexam);
+	}
+}
